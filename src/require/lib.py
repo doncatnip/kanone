a35df2a76fd166d1938ed92f8d062feb3cf1a503
Steffen Schulz <don.t@pan1.cc>
2011-04-25 05:03:36 +0100

    add utf-8 file headers

--- src/require/lib.py
+++ src/require/lib.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from zope.interface.advice import addClassAdvisor
 import sys
 
--- src/require/validator/schema.py
+++ src/require/validator/schema.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from ..lib import PASS, MISSING, messages, inherit
 from ..error import Invalid
 
