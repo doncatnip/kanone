cb2cfd355b849d694257f989de592dec15086981
doncatnip <don.t@pan1.cc>
2011-11-22 00:10:37 +0000

    add README.txt for pypi

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ import subprocess, shlex
 
 setup(
     name = 'kanone',
-    version = '0.2',
+    version = '0.3',
     description='a validation library',
+    description_long=open('README.txt').read(),
     author = 'don`catnip',
     author_email = 'don dot t at pan1 dot cc',
-    url = 'http://github.com/doncatnip/require',
+    url = 'http://github.com/doncatnip/kanone',
     packages = find_packages('src'),
     package_dir={'':'src'},
     install_requires = [ "zope.interface" ],
-    #namespace_packages=['require' ],
+    #namespace_packages=['kanone' ],
     include_package_data=True,
 )
