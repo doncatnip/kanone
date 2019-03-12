58b129e60bca978f90000666cba05e3c13d05ae9
Steffen Schulz <steffen.schulz@screengmbh.de>
2019-03-12 15:41:35 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup\
     ( name = 'kanone'
-    , version = '0.4.1'
+    , version = '0.4.3'
     , description = 'a validation library'
     , long_description = open('README.txt').read()
     , author = 'don`catnip'
